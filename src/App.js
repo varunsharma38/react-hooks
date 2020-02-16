bf2f11cc25191b5d83d276edfbbd872d79ec9a30
Varun Sharma <varunsharma38@live.com>
2020-02-16 22:48:12 +0530

    simple todo list with hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import "./App.scss";
 
-export default class App extends React.Component {
+export default function App() {
 
-    render() {
-        return (
-            <div>
-                <h3>React Webpack Test App</h3>
-            </div>
-        );
+    const [items, setItems] = useState([])
+    const [input, setInput] = useState('')
+
+    function handleClick() {
+        setItems([...items, input])
+        setInput('')
     }
+
+    function handleInput(e) {
+        setInput(e.target.value)
+    }
+
+    useEffect(() => {
+        document.title = `TODO List: ${items.length} items`
+    })
+
+    return (
+        <div className="todo-container">
+            <input type="text" value={input} onChange={handleInput} />
+            <button className="btn-primary" onClick={handleClick}>Add Item</button>
+            <ul className="item-list">
+                {items.map(item => <li key="item">{item}</li>)}
+            </ul>
+        </div>
+    )
 }
